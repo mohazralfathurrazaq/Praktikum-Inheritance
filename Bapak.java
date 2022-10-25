8bcf35bd23daf3c983a24b08b5a96423234542cf
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-10-25 23:05:02 +0700

    Add files via upload

---
+++ Bapak.java
@@ -0,0 +1,11 @@
+
+public class Bapak {
+    int a;
+    int b;
+    
+    public void show_variabel(){
+        System.out.println("Nilai a="+a);
+        System.out.println("Nilai b="+b);
+    }
+    
+}
