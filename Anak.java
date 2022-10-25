60211ed22507bc9b22d320878a05b0c9c73753c2
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-10-25 22:58:38 +0700

    Add files via upload

---
+++ Anak.java
@@ -0,0 +1,10 @@
+
+public class Anak extends Bapak {
+    int c;
+public void show_variabel(){
+    System.out.println("Nilai a="+ super.a);
+    System.out.println("Nilai b="+ super.b);
+    System.out.println("Nilai c="+ c);
+}
+    
+}
