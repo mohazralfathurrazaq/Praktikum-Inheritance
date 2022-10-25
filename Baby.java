bd441d99062d3936b64b317c22b01af2e35d4114
mohazralfathurrazaq <114301881+mohazralfathurrazaq@users.noreply.github.com>
2022-10-25 23:02:59 +0700

    Add files via upload

---
+++ Baby.java
@@ -0,0 +1,23 @@
+
+import javafx.scene.Parent;
+
+
+public class Baby extends Parent {
+    String babyName;
+    
+    public String getBabyName(){
+        return babyName;
+    }
+    Baby(String babyName){
+        super();
+        this.babyName = babyName;
+        System.out.println("Kontructor Baby");
+        System.out.println(babyName);
+    }
+    
+    public void cry(){
+        System.out.println("owek owek");
+    }
+    
+    
+}
